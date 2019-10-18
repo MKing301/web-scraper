fdac5cf30b264652b8becbf225608b734dadcb5a
MKING301 <33675411+MKing301@users.noreply.github.com>
2019-10-17 22:11:46 -0400

    Add try except blocks

--- app.py
+++ app.py
@@ -9,23 +9,28 @@ import requests
 import csv
 
 # Request to get text from web page
-source = requests.get('http://quotes.toscrape.com/tag/inspirational/').text
+try:
+    source = requests.get('httpw://quotes.toscrape.com/tag/inspirational/').text
 
-# Pass source into Beautiful Soup and parse html
-soup = BeautifulSoup(source, 'lxml')
+    # Pass source into Beautiful Soup and parse html
+    soup = BeautifulSoup(source, 'lxml')
 
-csv_file = open('quotes.csv', 'w')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Quote', 'Author'])
+    csv_file = open('quotes.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Quote', 'Author'])
 
-# Obain all quotes in the parsed html by looping through list of matching tags
-for quote in soup.find_all('div', class_='quote'):
+    # Obain all quotes in the parsed html by looping through list of matching tags
+    for quote in soup.find_all('div', class_='quote'):
 
-    # Obtain quote
-    quote_text = quote.find('span', class_='text').text
+        # Obtain quote
+        quote_text = quote.find('span', class_='text').text
 
-    # Obtain auther of the quote
-    quote_author = quote.find('small', class_='author').text
+        # Obtain auther of the quote
+        quote_author = quote.find('small', class_='author').text
 
-    csv_writer.writerow([quote_text, quote_author])
-csv_file.close()
+        csv_writer.writerow([quote_text, quote_author])
+        
+    csv_file.close()
+
+except Exception as e:
+    print(f'Exception: {e}')
